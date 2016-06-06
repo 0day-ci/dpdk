a1ddb5a65f7d07f533adda79d6fab9e7a757fa3b
Ajit Khaparde <ajit.khaparde@broadcom.com>
2016-06-06 15:08:26 -0700

    bnxt: add API for L2 Rx mask set/clear functions
    These HWRM APIs allow setting and clearing of Rx masks in L2 context
    per VNIC.
    
    v4:
    Address review comments.
    
    Signed-off-by: Ajit Khaparde <ajit.khaparde@broadcom.com>
    Reviewed-by: David Christensen <david.christensen@broadcom.com>
    Signed-off-by: Stephen Hurd <stephen.hurd@broadcom.com>

--- drivers/net/bnxt/bnxt_hwrm.h
+++ drivers/net/bnxt/bnxt_hwrm.h
@@ -42,6 +42,9 @@
 struct bnxt;
 struct bnxt_filter_info;
 struct bnxt_cp_ring_info;
+int bnxt_hwrm_cfa_l2_clear_rx_mask(struct bnxt *bp,
+				   struct bnxt_vnic_info *vnic);
+int bnxt_hwrm_cfa_l2_set_rx_mask(struct bnxt *bp, struct bnxt_vnic_info *vnic);
 int bnxt_hwrm_clear_filter(struct bnxt *bp,
 			   struct bnxt_filter_info *filter);
 
