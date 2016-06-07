0day-ci/dpdk

commit 45657a5c6861a7bea4041eb2d0e7d7179904336c

examples/vhost: use tailq to link vhost devices
To simplify code and logic.

Signed-off-by: Yuanhan Liu <yuanhan.liu@linux.intel.com>

examples/vhost/main.h
@@ -1,7 +1,7 @@
 /*-
  *   BSD LICENSE
  *
- *   Copyright(c) 2010-2014 Intel Corporation. All rights reserved.
+ *   Copyright(c) 2010-2016 Intel Corporation. All rights reserved.
  *   All rights reserved.
  *
  *   Redistribution and use in source and binary forms, with or without
@@ -34,6 +34,8 @@
 #ifndef _MAIN_H_
 #define _MAIN_H_
 
+#include <sys/queue.h>
+
 /* Macros for printing using RTE_LOG */
 #define RTE_LOGTYPE_VHOST_CONFIG RTE_LOGTYPE_USER1
 #define RTE_LOGTYPE_VHOST_DATA   RTE_LOGTYPE_USER2
@@ -59,28 +61,26 @@ struct vhost_dev {
 	volatile uint8_t ready;
 	/**< Device is marked for removal from the data core. */
 	volatile uint8_t remove;
+
+	TAILQ_ENTRY(vhost_dev) next;
 } __rte_cache_aligned;
 
-struct virtio_net_data_ll
-{
-	struct vhost_dev		*vdev;	/* Pointer to device created by configuration core. */
-	struct virtio_net_data_ll	*next;  /* Pointer to next device in linked list. */
-};
+TAILQ_HEAD(vhost_dev_tailq_list, vhost_dev);
+
+
+#define REQUEST_DEV_REMOVAL	1
+#define ACK_DEV_REMOVAL		0
 
 /*
  * Structure containing data core specific information.
  */
-struct lcore_ll_info
-{
-	struct virtio_net_data_ll	*ll_root_free; 		/* Pointer to head in free linked list. */
-	struct virtio_net_data_ll	*ll_root_used;		/* Pointer to head of used linked list. */
-	uint32_t 					device_num;			/* Number of devices on lcore. */
-	volatile uint8_t			dev_removal_flag;	/* Flag to synchronize device removal. */
-};
+struct lcore_info {
+	uint32_t		device_num;
 
-struct lcore_info
-{
-	struct lcore_ll_info	*lcore_ll;	/* Pointer to data core specific lcore_ll_info struct */
+	/* Flag to synchronize device removal. */
+	volatile uint8_t	dev_removal_flag;
+
+	struct vhost_dev_tailq_list vdev_list;
 };
 
 #endif /* _MAIN_H_ */

commit 637f73f36b691eecaa36cc4fc0229a02655390cc

examples/vhost: fix corrupted vdev tailq list
There are two tailq lists, one for logging all vhost devices, another
one for logging vhost devices distributed on a specific core. However,
there is just one tailq entry, named "next", to chain the two list,
which is wrong and could result to a corrupted tailq list, that the
tailq list might always be non-empty: the entry is still there even
after you have invoked TAILQ_REMOVE several times.

Fix it by introducing two tailq entries, one for each list.

Fixes: 45657a5c6861 ("examples/vhost: use tailq to link vhost devices")

Signed-off-by: Yuanhan Liu <yuanhan.liu@linux.intel.com>

examples/vhost/main.c
@@ -691,7 +691,7 @@ find_vhost_dev(struct ether_addr *mac)
 {
 	struct vhost_dev *vdev;
 
-	TAILQ_FOREACH(vdev, &vhost_dev_list, next) {
+	TAILQ_FOREACH(vdev, &vhost_dev_list, global_vdev_entry) {
 		if (vdev->ready == DEVICE_RX &&
 		    is_same_ether_addr(mac, &vdev->mac_address))
 			return vdev;
@@ -945,7 +945,7 @@ virtio_tx_route(struct vhost_dev *vdev, struct rte_mbuf *m, uint16_t vlan_tag)
 	if (unlikely(is_broadcast_ether_addr(&nh->d_addr))) {
 		struct vhost_dev *vdev2;
 
-		TAILQ_FOREACH(vdev2, &vhost_dev_list, next) {
+		TAILQ_FOREACH(vdev2, &vhost_dev_list, global_vdev_entry) {
 			virtio_xmit(vdev2, vdev, m);
 		}
 		goto queue2nic;
@@ -1149,7 +1149,8 @@ switch_worker(void *arg __rte_unused)
 		/*
 		 * Process vhost devices
 		 */
-		TAILQ_FOREACH(vdev, &lcore_info[lcore_id].vdev_list, next) {
+		TAILQ_FOREACH(vdev, &lcore_info[lcore_id].vdev_list,
+			      lcore_vdev_entry) {
 			if (unlikely(vdev->remove)) {
 				unlink_vmdq(vdev);
 				vdev->ready = DEVICE_SAFE_REMOVE;
@@ -1188,8 +1189,10 @@ destroy_device (volatile struct virtio_net *dev)
 		rte_pause();
 	}
 
-	TAILQ_REMOVE(&lcore_info[vdev->coreid].vdev_list, vdev, next);
-	TAILQ_REMOVE(&vhost_dev_list, vdev, next);
+	TAILQ_REMOVE(&lcore_info[vdev->coreid].vdev_list, vdev,
+		     lcore_vdev_entry);
+	TAILQ_REMOVE(&vhost_dev_list, vdev, global_vdev_entry);
+
 
 	/* Set the dev_removal_flag on each lcore. */
 	RTE_LCORE_FOREACH_SLAVE(lcore)
@@ -1234,7 +1237,7 @@ new_device (struct virtio_net *dev)
 	vdev->dev = dev;
 	dev->priv = vdev;
 
-	TAILQ_INSERT_TAIL(&vhost_dev_list, vdev, next);
+	TAILQ_INSERT_TAIL(&vhost_dev_list, vdev, global_vdev_entry);
 	vdev->vmdq_rx_q
 		= dev->device_fh * queues_per_pool + vmdq_queue_base;
 
@@ -1251,7 +1254,8 @@ new_device (struct virtio_net *dev)
 	}
 	vdev->coreid = core_add;
 
-	TAILQ_INSERT_TAIL(&lcore_info[vdev->coreid].vdev_list, vdev, next);
+	TAILQ_INSERT_TAIL(&lcore_info[vdev->coreid].vdev_list, vdev,
+			  lcore_vdev_entry);
 	lcore_info[vdev->coreid].device_num++;
 
 	/* Disable notifications. */
@@ -1294,7 +1298,7 @@ print_stats(void)
 		printf("%s%s\n", clr, top_left);
 		printf("Device statistics =================================\n");
 
-		TAILQ_FOREACH(vdev, &vhost_dev_list, next) {
+		TAILQ_FOREACH(vdev, &vhost_dev_list, global_vdev_entry) {
 			tx_total   = vdev->stats.tx_total;
 			tx         = vdev->stats.tx;
 			tx_dropped = tx_total - tx;

examples/vhost/main.h
@@ -67,7 +67,8 @@ struct vhost_dev {
 	volatile uint8_t remove;
 
 	struct device_statistics stats;
-	TAILQ_ENTRY(vhost_dev) next;
+	TAILQ_ENTRY(vhost_dev) global_vdev_entry;
+	TAILQ_ENTRY(vhost_dev) lcore_vdev_entry;
 } __rte_cache_aligned;
 
 TAILQ_HEAD(vhost_dev_tailq_list, vhost_dev);