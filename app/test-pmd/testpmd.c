d35740f9ca90a6ddba98a0e0649f2ddca4969d41
Pablo de Lara <pablo.de.lara.guarch@intel.com>
2016-06-08 16:20:31 +0100

    testpmd: fix typo when closing port
    Fixes: ce8d561418d4 ("app/testpmd: add port configuration settings")
    
    Signed-off-by: Pablo de Lara <pablo.de.lara.guarch@intel.com>

--- app/test-pmd/testpmd.c
+++ app/test-pmd/testpmd.c
@@ -1502,7 +1502,7 @@ close_port(portid_t pid)
 
 		if (rte_atomic16_cmpset(&(port->port_status),
 			RTE_PORT_HANDLING, RTE_PORT_CLOSED) == 0)
-			printf("Port %d can not be set into stopped\n", pi);
+			printf("Port %d can not be set into closed\n", pi);
 	}
 
 	printf("Done\n");
