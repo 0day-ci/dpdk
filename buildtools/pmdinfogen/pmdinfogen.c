0day-ci/dpdk

commit 840e5dfea3f8216ecbc07cf5570a9fbfd4fcfbbd

pmdinfogen: fix usage message
The name of the tool is pmdinfogen.

Fixes: 98b0fdb0ffc6 ("pmdinfogen: add buildtools and pmdinfogen utility")

Signed-off-by: Thomas Monjalon <thomas.monjalon@6wind.com>
Acked-by: Neil Horman <nhorman@tuxdriver.com>

buildtools/pmdinfogen/pmdinfogen.c
@@ -398,7 +398,8 @@ int main(int argc, char **argv)
 
 	if (argc < 3) {
 		fprintf(stderr,
-			"usage: pmdinfo <object file> <c output file>\n");
+			"usage: %s <object file> <c output file>\n",
+			basename(argv[0]));
 		exit(127);
 	}
 	parse_elf(&info, argv[1]);

commit 2d43155be8a01e582d1f3c7f70be3fa944e47116

pmdinfogen: fix build on FreeBSD
error on compilation caused by missing include for libgen.h.
  HOSTCC pmdinfogen.o
/usr/home/bruce/dpdk/buildtools/pmdinfogen/pmdinfogen.c:402:4: error: implicit declaration of function 'basename' is invalid in C99
      [-Werror,-Wimplicit-function-declaration]
                        basename(argv[0]));

Fixes: 840e5dfea3f8 ("pmdinfogen: fix usage message")

Signed-off-by: Bruce Richardson <bruce.richardson@intel.com>

buildtools/pmdinfogen/pmdinfogen.c
@@ -15,6 +15,8 @@
 #include <limits.h>
 #include <stdbool.h>
 #include <errno.h>
+#include <libgen.h>
+
 #include <rte_common.h>
 #include "pmdinfogen.h"
 