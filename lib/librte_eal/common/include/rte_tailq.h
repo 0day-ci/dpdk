d5169b4d67398a8bbc875d6649048d3e400f2c1f
Sergio Gonzalez Monroy <sergio.gonzalez.monroy@intel.com>
2016-07-22 17:01:17 +0100

    eal: add stailq safe iterator macro
    Removing/freeing elements elements within a STAILQ_FOREACH loop
    is not safe. FreeBSD defines STAILQ_FOREACH_SAFE macro, which permits
    these operations safely.
    
    This patch defines this macro for Linux systems, where it is not defined.
    
    Signed-off-by: Sergio Gonzalez Monroy <sergio.gonzalez.monroy@intel.com>

--- lib/librte_eal/common/include/rte_tailq.h
+++ lib/librte_eal/common/include/rte_tailq.h
@@ -163,6 +163,13 @@ void __attribute__((constructor, used)) tailqinitfn_ ##t(void) \
 	    (var) = (tvar))
 #endif
 
+#ifndef SLIST_FOREACH_SAFE
+#define SLIST_FOREACH_SAFE(var, head, field, tvar)                      \
+	for ((var) = SLIST_FIRST((head));                               \
+	    (var) && ((tvar) = SLIST_NEXT((var), field), 1);            \
+	    (var) = (tvar))
+#endif
+
 #ifdef __cplusplus
 }
 #endif
