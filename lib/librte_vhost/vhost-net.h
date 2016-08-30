38f838fc036c67a5645f849bac6385e3e9738d95
Zhihong Wang <zhihong.wang@intel.com>
2016-08-29 23:36:01 -0400

    vhost: remove useless volatile
    This patch removes useless volatile attribute to allow compiler
    optimization.
    
    Signed-off-by: Zhihong Wang <zhihong.wang@intel.com>

--- lib/librte_vhost/vhost-net.h
+++ lib/librte_vhost/vhost-net.h
@@ -71,7 +71,7 @@ struct vhost_virtqueue {
 	uint32_t		size;
 
 	/* Last index used on the available ring */
-	volatile uint16_t	last_used_idx;
+	uint16_t		last_used_idx;
 #define VIRTIO_INVALID_EVENTFD		(-1)
 #define VIRTIO_UNINITIALIZED_EVENTFD	(-2)
 
