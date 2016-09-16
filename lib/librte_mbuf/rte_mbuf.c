7b2971cdf96e0390349147943ea76e87457e3d1d
Hemant Agrawal <hemant.agrawal@nxp.com>
2016-09-16 22:16:35 +0530

    mempool: pktmbuf pool default fallback for mempool ops error
    In the rte_pktmbuf_pool_create, if the default external mempool is
    not available, the implementation can default to "ring_mp_mc", which
    is an software implementation.
    
    Signed-off-by: Hemant Agrawal <hemant.agrawal@nxp.com>

--- lib/librte_mbuf/rte_mbuf.c
+++ lib/librte_mbuf/rte_mbuf.c
@@ -176,6 +176,14 @@ rte_pktmbuf_pool_create(const char *name, unsigned n,
 
 	rte_errno = rte_mempool_set_ops_byname(mp,
 			RTE_MBUF_DEFAULT_MEMPOOL_OPS, NULL);
+
+	/* on error, try falling back to the software based default pool */
+	if (rte_errno == -EOPNOTSUPP) {
+		RTE_LOG(WARNING, MBUF, "Default HW Mempool not supported. "
+			"falling back to sw mempool \"ring_mp_mc\"");
+		rte_errno = rte_mempool_set_ops_byname(mp, "ring_mp_mc", NULL);
+	}
+
 	if (rte_errno != 0) {
 		RTE_LOG(ERR, MBUF, "error setting mempool handler\n");
 		return NULL;
