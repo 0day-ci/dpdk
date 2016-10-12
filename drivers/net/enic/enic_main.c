bc5a6d6417b64e805205eaac75af55f53f862872
John Daley <johndale@cisco.com>
2016-10-12 14:22:29 -0700

    net/enic: fix poor multi-queue Rx performance
    The rx_free_thresh was not being initialized and left at 0
    on 1/2 of the RQs which could lead to poor multi-queue
    performanace.
    
    Fixes: 856d7ba7ed22 ("net/enic: support scattered Rx")
    
    Signed-off-by: John Daley <johndale@cisco.com>
    Reviewed-by: Nelson Escobar <neescoba@cisco.com>

--- drivers/net/enic/enic_main.c
+++ drivers/net/enic/enic_main.c
@@ -603,7 +603,7 @@ int enic_stop_rq(struct enic *enic, uint16_t queue_idx)
 
 int enic_alloc_rq(struct enic *enic, uint16_t queue_idx,
 	unsigned int socket_id, struct rte_mempool *mp,
-	uint16_t nb_desc)
+	uint16_t nb_desc, uint16_t free_thresh)
 {
 	int rc;
 	uint16_t sop_queue_idx = enic_sop_rq(queue_idx);
@@ -624,6 +624,10 @@ int enic_alloc_rq(struct enic *enic, uint16_t queue_idx,
 	rq_data->socket_id = socket_id;
 	rq_data->mp = mp;
 	rq_sop->in_use = 1;
+	rq_sop->rx_free_thresh = free_thresh;
+	rq_data->rx_free_thresh = free_thresh;
+	dev_debug(enic, "Set queue_id:%u free thresh:%u\n", queue_idx,
+		  free_thresh);
 
 	mbuf_size = (uint16_t)(rte_pktmbuf_data_room_size(mp) -
 			       RTE_PKTMBUF_HEADROOM);
@@ -1236,7 +1240,7 @@ int enic_set_mtu(struct enic *enic, uint16_t new_mtu)
 
 		enic_free_rq(rq);
 		rc = enic_alloc_rq(enic, rq_idx, rq->socket_id, rq->mp,
-				   rq->tot_nb_desc);
+				   rq->tot_nb_desc, rq->rx_free_thresh);
 		if (rc) {
 			dev_err(enic,
 				"Fatal MTU alloc error- No traffic will pass\n");
