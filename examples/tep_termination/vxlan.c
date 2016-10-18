0day-ci/dpdk

commit 2bb43bd4350a6f5060add5436b1f44e08b936508

examples/tep_term: add TSO offload configuration
Add the tso-segsz option for TSO offload.

If the 'tso-segsz' is not 0, it means TSO offload is enabled.

Signed-off-by: Jijiang Liu <jijiang.liu@intel.com>

examples/tep_termination/vxlan.c
@@ -144,6 +144,11 @@ process_inner_cksums(struct ether_hdr *eth_hdr, union tunnel_offload_info *info)
 		ol_flags |= PKT_TX_TCP_CKSUM;
 		tcp_hdr->cksum = get_psd_sum(l3_hdr, ethertype,
 				ol_flags);
+		if (tso_segsz != 0) {
+			ol_flags |= PKT_TX_TCP_SEG;
+			info->tso_segsz = tso_segsz;
+			info->l4_len = sizeof(struct tcp_hdr);
+		}
 
 	} else if (l4_proto == IPPROTO_SCTP) {
 		sctp_hdr = (struct sctp_hdr *)((char *)l3_hdr + info->l3_len);
@@ -226,6 +231,7 @@ encapsulation(struct rte_mbuf *m, uint8_t queue_id)
 		ol_flags |= process_inner_cksums(phdr, &tx_offload);
 		m->l2_len = tx_offload.l2_len;
 		m->l3_len = tx_offload.l3_len;
+		m->l4_len = tx_offload.l4_len;
 		m->l2_len += ETHER_VXLAN_HLEN;
 	}
 
@@ -233,6 +239,7 @@ encapsulation(struct rte_mbuf *m, uint8_t queue_id)
 	m->outer_l3_len = sizeof(struct ipv4_hdr);
 
 	m->ol_flags |= ol_flags;
+	m->tso_segsz = tx_offload.tso_segsz;
 
 	/*VXLAN HEADER*/
 	vxlan->vx_flags = rte_cpu_to_be_32(VXLAN_HF_VNI);

examples/tep_termination/main.c
@@ -114,6 +114,7 @@
 #define CMD_LINE_OPT_NB_DEVICES "nb-devices"
 #define CMD_LINE_OPT_UDP_PORT "udp-port"
 #define CMD_LINE_OPT_TX_CHECKSUM "tx-checksum"
+#define CMD_LINE_OPT_TSO_SEGSZ "tso-segsz"
 #define CMD_LINE_OPT_FILTER_TYPE "filter-type"
 #define CMD_LINE_OPT_RX_RETRY "rx-retry"
 #define CMD_LINE_OPT_RX_RETRY_DELAY "rx-retry-delay"
@@ -144,6 +145,9 @@ uint16_t udp_port = 4789;
 
 /* enable/disable inner TX checksum */
 uint8_t tx_checksum = 0;
+
+/* TCP segment size */
+uint16_t tso_segsz = 0;
 
 /* RX filter type for tunneling packet */
 uint8_t filter_idx = 1;
@@ -265,6 +269,7 @@ tep_termination_usage(const char *prgname)
 	"               --udp-port: UDP destination port for VXLAN packet\n"
 	"		--nb-devices[1-64]: The number of virtIO device\n"
 	"               --tx-checksum [0|1]: inner Tx checksum offload\n"
+	"               --tso-segsz [0-N]: TCP segment size\n"
 	"               --filter-type[1-3]: filter type for tunneling packet\n"
 	"                   1: Inner MAC and tenent ID\n"
 	"                   2: Inner MAC and VLAN, and tenent ID\n"
@@ -295,6 +300,7 @@ tep_termination_parse_args(int argc, char **argv)
 		{CMD_LINE_OPT_NB_DEVICES, required_argument, NULL, 0},
 		{CMD_LINE_OPT_UDP_PORT, required_argument, NULL, 0},
 		{CMD_LINE_OPT_TX_CHECKSUM, required_argument, NULL, 0},
+		{CMD_LINE_OPT_TSO_SEGSZ, required_argument, NULL, 0},
 		{CMD_LINE_OPT_FILTER_TYPE, required_argument, NULL, 0},
 		{CMD_LINE_OPT_RX_RETRY, required_argument, NULL, 0},
 		{CMD_LINE_OPT_RX_RETRY_DELAY, required_argument, NULL, 0},
@@ -345,6 +351,19 @@ tep_termination_parse_args(int argc, char **argv)
 					return -1;
 				} else
 					enable_retry = ret;
+			}
+
+			if (!strncmp(long_option[option_index].name,
+				CMD_LINE_OPT_TSO_SEGSZ,
+				sizeof(CMD_LINE_OPT_TSO_SEGSZ))) {
+				ret = parse_num_opt(optarg, INT16_MAX);
+				if (ret == -1) {
+					RTE_LOG(INFO, VHOST_CONFIG,
+						"Invalid argument for TCP segment size [0-N]\n");
+					tep_termination_usage(prgname);
+					return -1;
+				} else
+					tso_segsz = ret;
 			}
 
 			if (!strncmp(long_option[option_index].name,

examples/tep_termination/vxlan.h
@@ -48,6 +48,7 @@
 extern struct ipv4_hdr app_ip_hdr[VXLAN_N_PORTS];
 extern struct ether_hdr app_l2_hdr[VXLAN_N_PORTS];
 extern uint8_t tx_checksum;
+extern uint16_t tso_segsz;
 
 struct vxlan_port {
 	uint32_t vport_id;           /**< VirtIO port id */
@@ -73,6 +74,7 @@ union tunnel_offload_info {
 		uint64_t l2_len:7; /**< L2 (MAC) Header Length. */
 		uint64_t l3_len:9; /**< L3 (IP) Header Length. */
 		uint64_t l4_len:8; /**< L4 Header Length. */
+		uint64_t tso_segsz:16; /**< TCP TSO segment size */
 		uint64_t outer_l2_len:7; /**< outer L2 Header Length */
 		uint64_t outer_l3_len:16; /**< outer L3 Header Length */
 	};

examples/tep_termination/vxlan_setup.c
@@ -205,6 +205,13 @@ vxlan_port_init(uint8_t port, struct rte_mempool *mbuf_pool)
 			ports_eth_addr[port].addr_bytes[4],
 			ports_eth_addr[port].addr_bytes[5]);
 
+	if (tso_segsz != 0) {
+		struct rte_eth_dev_info dev_info;
+		rte_eth_dev_info_get(port, &dev_info);
+		if ((dev_info.tx_offload_capa & DEV_TX_OFFLOAD_TCP_TSO) == 0)
+			RTE_LOG(WARNING, PORT,
+				"hardware TSO offload is not supported\n");
+	}
 	return 0;
 }
 

commit ada0309d10e6f14b0561e869b2d17688ab3f5393

examples/tep_term: Fix l4_len issue
l4_len is not fixed, althrough mostly it is a fixed value,
but when guest using iperf to do some tests, the l4_len
will have another 12 bytes optional fields.

Fixes: 2bb43bd4350a("examples/tep_term: add TSO offload configuration")

Signed-off-by: Michael Qiu <qiudayu@chinac.com>

examples/tep_termination/vxlan.c
@@ -147,7 +147,7 @@ process_inner_cksums(struct ether_hdr *eth_hdr, union tunnel_offload_info *info)
 		if (tso_segsz != 0) {
 			ol_flags |= PKT_TX_TCP_SEG;
 			info->tso_segsz = tso_segsz;
-			info->l4_len = sizeof(struct tcp_hdr);
+			info->l4_len = (tcp_hdr->data_off & 0xf0) >> 2;
 		}
 
 	} else if (l4_proto == IPPROTO_SCTP) {