0day-ci/dpdk

commit 0497ddaac511ea86a4d7f5239a723512fc176a98

mlx5: add special flows for broadcast and IPv6 multicast
Until now, broadcast frames were handled like unicast. Moving the related
flow to the special flows table frees up the related unicast MAC entry.

The same method is used to handle IPv6 multicast frames.

Signed-off-by: Yaacov Hazan <yaacovh@mellanox.com>
Signed-off-by: Adrien Mazarguil <adrien.mazarguil@6wind.com>

drivers/net/mlx5/mlx5_rxmode.c
@@ -88,6 +88,30 @@ static const struct special_flow_init special_flow_init[] = {
 			1 << HASH_RXQ_ETH |
 			0,
 	},
+	[HASH_RXQ_FLOW_TYPE_BROADCAST] = {
+		.dst_mac_val = "\xff\xff\xff\xff\xff\xff",
+		.dst_mac_mask = "\xff\xff\xff\xff\xff\xff",
+		.hash_types =
+			1 << HASH_RXQ_UDPV4 |
+			1 << HASH_RXQ_IPV4 |
+#ifdef HAVE_FLOW_SPEC_IPV6
+			1 << HASH_RXQ_UDPV6 |
+			1 << HASH_RXQ_IPV6 |
+#endif /* HAVE_FLOW_SPEC_IPV6 */
+			1 << HASH_RXQ_ETH |
+			0,
+	},
+#ifdef HAVE_FLOW_SPEC_IPV6
+	[HASH_RXQ_FLOW_TYPE_IPV6MULTI] = {
+		.dst_mac_val = "\x33\x33\x00\x00\x00\x00",
+		.dst_mac_mask = "\xff\xff\x00\x00\x00\x00",
+		.hash_types =
+			1 << HASH_RXQ_UDPV6 |
+			1 << HASH_RXQ_IPV6 |
+			1 << HASH_RXQ_ETH |
+			0,
+	},
+#endif /* HAVE_FLOW_SPEC_IPV6 */
 };
 
 /**

drivers/net/mlx5/mlx5.c
@@ -90,6 +90,8 @@ mlx5_dev_close(struct rte_eth_dev *dev)
 	priv_dev_interrupt_handler_uninstall(priv, dev);
 	priv_special_flow_disable(priv, HASH_RXQ_FLOW_TYPE_ALLMULTI);
 	priv_special_flow_disable(priv, HASH_RXQ_FLOW_TYPE_PROMISC);
+	priv_special_flow_disable(priv, HASH_RXQ_FLOW_TYPE_BROADCAST);
+	priv_special_flow_disable(priv, HASH_RXQ_FLOW_TYPE_IPV6MULTI);
 	priv_mac_addrs_disable(priv);
 	priv_destroy_hash_rxqs(priv);
 	/* Prevent crashes when queues are still in use. */
@@ -416,13 +418,10 @@ mlx5_pci_devinit(struct rte_pci_driver *pci_drv, struct rte_pci_device *pci_dev)
 		     mac.addr_bytes[0], mac.addr_bytes[1],
 		     mac.addr_bytes[2], mac.addr_bytes[3],
 		     mac.addr_bytes[4], mac.addr_bytes[5]);
-		/* Register MAC and broadcast addresses. */
+		/* Register MAC address. */
 		claim_zero(priv_mac_addr_add(priv, 0,
 					     (const uint8_t (*)[ETHER_ADDR_LEN])
 					     mac.addr_bytes));
-		claim_zero(priv_mac_addr_add(priv, (RTE_DIM(priv->mac) - 1),
-					     &(const uint8_t [ETHER_ADDR_LEN])
-					     { "\xff\xff\xff\xff\xff\xff" }));
 #ifndef NDEBUG
 		{
 			char ifname[IF_NAMESIZE];

drivers/net/mlx5/mlx5_defs.h
@@ -44,7 +44,7 @@
 #define MLX5_MAX_VLAN_IDS 128
 
 /* Maximum number of special flows. */
-#define MLX5_MAX_SPECIAL_FLOWS 2
+#define MLX5_MAX_SPECIAL_FLOWS 4
 
 /* Request send completion once in every 64 sends, might be less. */
 #define MLX5_PMD_TX_PER_COMP_REQ 64

drivers/net/mlx5/mlx5_ethdev.c
@@ -501,8 +501,7 @@ mlx5_dev_infos_get(struct rte_eth_dev *dev, struct rte_eth_dev_info *info)
 		max = 65535;
 	info->max_rx_queues = max;
 	info->max_tx_queues = max;
-	/* Last array entry is reserved for broadcast. */
-	info->max_mac_addrs = (RTE_DIM(priv->mac) - 1);
+	info->max_mac_addrs = RTE_DIM(priv->mac);
 	info->rx_offload_capa =
 		(priv->hw_csum ?
 		 (DEV_RX_OFFLOAD_IPV4_CKSUM |

drivers/net/mlx5/mlx5_mac.c
@@ -212,8 +212,7 @@ mlx5_mac_addr_remove(struct rte_eth_dev *dev, uint32_t index)
 	priv_lock(priv);
 	DEBUG("%p: removing MAC address from index %" PRIu32,
 	      (void *)dev, index);
-	/* Last array entry is reserved for broadcast. */
-	if (index >= (RTE_DIM(priv->mac) - 1))
+	if (index >= RTE_DIM(priv->mac))
 		goto end;
 	priv_mac_addr_del(priv, index);
 end:
@@ -479,8 +478,7 @@ mlx5_mac_addr_add(struct rte_eth_dev *dev, struct ether_addr *mac_addr,
 	priv_lock(priv);
 	DEBUG("%p: adding MAC address at index %" PRIu32,
 	      (void *)dev, index);
-	/* Last array entry is reserved for broadcast. */
-	if (index >= (RTE_DIM(priv->mac) - 1))
+	if (index >= RTE_DIM(priv->mac))
 		goto end;
 	priv_mac_addr_add(priv, index,
 			  (const uint8_t (*)[ETHER_ADDR_LEN])

drivers/net/mlx5/mlx5_rxq.c
@@ -579,8 +579,18 @@ priv_allow_flow_type(struct priv *priv, enum hash_rxq_flow_type type)
 		return !!priv->promisc_req;
 	case HASH_RXQ_FLOW_TYPE_ALLMULTI:
 		return !!priv->allmulti_req;
+	case HASH_RXQ_FLOW_TYPE_BROADCAST:
+#ifdef HAVE_FLOW_SPEC_IPV6
+	case HASH_RXQ_FLOW_TYPE_IPV6MULTI:
+#endif /* HAVE_FLOW_SPEC_IPV6 */
+		/* If allmulti is enabled, broadcast and ipv6multi
+		 * are unnecessary. */
+		return !priv->allmulti_req;
 	case HASH_RXQ_FLOW_TYPE_MAC:
 		return 1;
+	default:
+		/* Unsupported flow type is not allowed. */
+		return 0;
 	}
 	return 0;
 }

drivers/net/mlx5/mlx5_rxtx.h
@@ -186,6 +186,8 @@ struct special_flow_init {
 enum hash_rxq_flow_type {
 	HASH_RXQ_FLOW_TYPE_PROMISC,
 	HASH_RXQ_FLOW_TYPE_ALLMULTI,
+	HASH_RXQ_FLOW_TYPE_BROADCAST,
+	HASH_RXQ_FLOW_TYPE_IPV6MULTI,
 	HASH_RXQ_FLOW_TYPE_MAC,
 };
 
@@ -198,6 +200,10 @@ hash_rxq_flow_type_str(enum hash_rxq_flow_type flow_type)
 		return "promiscuous";
 	case HASH_RXQ_FLOW_TYPE_ALLMULTI:
 		return "allmulticast";
+	case HASH_RXQ_FLOW_TYPE_BROADCAST:
+		return "broadcast";
+	case HASH_RXQ_FLOW_TYPE_IPV6MULTI:
+		return "IPv6 multicast";
 	case HASH_RXQ_FLOW_TYPE_MAC:
 		return "MAC";
 	}

drivers/net/mlx5/mlx5_trigger.c
@@ -80,6 +80,8 @@ mlx5_dev_start(struct rte_eth_dev *dev)
 		      " %s",
 		      (void *)priv, strerror(err));
 		/* Rollback. */
+		priv_special_flow_disable(priv, HASH_RXQ_FLOW_TYPE_IPV6MULTI);
+		priv_special_flow_disable(priv, HASH_RXQ_FLOW_TYPE_BROADCAST);
 		priv_special_flow_disable(priv, HASH_RXQ_FLOW_TYPE_ALLMULTI);
 		priv_special_flow_disable(priv, HASH_RXQ_FLOW_TYPE_PROMISC);
 		priv_mac_addrs_disable(priv);
@@ -109,6 +111,8 @@ mlx5_dev_stop(struct rte_eth_dev *dev)
 		return;
 	}
 	DEBUG("%p: cleaning up and destroying hash RX queues", (void *)dev);
+	priv_special_flow_disable(priv, HASH_RXQ_FLOW_TYPE_IPV6MULTI);
+	priv_special_flow_disable(priv, HASH_RXQ_FLOW_TYPE_BROADCAST);
 	priv_special_flow_disable(priv, HASH_RXQ_FLOW_TYPE_ALLMULTI);
 	priv_special_flow_disable(priv, HASH_RXQ_FLOW_TYPE_PROMISC);
 	priv_mac_addrs_disable(priv);

commit df1460b0a34679b351e5b9d81e0e4eb5a2b24bad

net/mlx5: fix default set for multicast traffic
Remove non-IPv6 multicast traffic with destination MAC 33:33:* from the
default set when not in promiscuous or allmulticast modes.

Fixes: 0497ddaac511 ("mlx5: add special flows for broadcast and IPv6 multicast")

Signed-off-by: Raslan Darawsheh <rasland@mellanox.com>

drivers/net/mlx5/mlx5_rxmode.c
@@ -104,7 +104,6 @@ static const struct special_flow_init special_flow_init[] = {
 		.hash_types =
 			1 << HASH_RXQ_UDPV6 |
 			1 << HASH_RXQ_IPV6 |
-			1 << HASH_RXQ_ETH |
 			0,
 		.per_vlan = 1,
 	},