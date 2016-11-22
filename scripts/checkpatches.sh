0day-ci/dpdk

commit 8005feef421dc1fd01a0fa9fe611dbf8f41b211c

scripts: add standard input to checkpatch
It is now possible to check a patch by providing an email
through stdin.

Signed-off-by: Thomas Monjalon <thomas.monjalon@6wind.com>

scripts/checkpatches.sh
@@ -53,7 +53,7 @@ print_usage () {
 	Run Linux kernel checkpatch.pl with DPDK options.
 	The environment variable DPDK_CHECKPATCH_PATH must be set.
 
-	The patches to check can be from files specified on the command line,
+	The patches to check can be from stdin, files specified on the command line,
 	or latest git commits limited with -n option (default limit: origin/master).
 	END_OF_HELP
 }
@@ -90,6 +90,8 @@ check () { # <patch> <commit> <title>
 	elif [ -n "$2" ] ; then
 		report=$(git format-patch --no-stat --stdout -1 $commit |
 			$DPDK_CHECKPATCH_PATH $options - 2>/dev/null)
+	else
+		report=$($DPDK_CHECKPATCH_PATH $options - 2>/dev/null)
 	fi
 	[ $? -ne 0 ] || continue
 	$verbose || printf '\n### %s\n\n' "$3"
@@ -97,7 +99,20 @@ check () { # <patch> <commit> <title>
 	status=$(($status + 1))
 }
 
-if [ -z "$1" ] ; then
+if [ -n "$1" ] ; then
+	for patch in "$@" ; do
+		subject=$(sed -n 's,^Subject: ,,p' "$patch")
+		check "$patch" '' "$subject"
+	done
+elif [ ! -t 0 ] ; then # stdin
+	subject=$(while read header value ; do
+		if [ "$header" = 'Subject:' ] ; then
+			echo $value
+			break
+		fi
+	done)
+	check '' '' "$subject"
+else
 	if [ $number -eq 0 ] ; then
 		commits=$(git rev-list --reverse origin/master..)
 	else
@@ -106,11 +121,6 @@ if [ -z "$1" ] ; then
 	for commit in $commits ; do
 		subject=$(git log --format='%s' -1 $commit)
 		check '' $commit "$subject"
-	done
-else
-	for patch in "$@" ; do
-		subject=$(sed -n 's,^Subject: ,,p' "$patch")
-		check "$patch" '' "$subject"
 	done
 fi
 pass=$(($total - $status))

commit 8816ccd9adb05b398ea39f0fb50f44b13fd67b39

scripts: fix checkpatch from standard input
When checking a valid patch from standard input,
the footer lines of the report are not filtered out.

The function check is called outside of any loop,
so the statement continue has no effect and the footer is printed.

Fixes: 8005feef421d ("scripts: add standard input to checkpatch")

Signed-off-by: Thomas Monjalon <thomas.monjalon@6wind.com>

scripts/checkpatches.sh
@@ -94,7 +94,7 @@ check () { # <patch> <commit> <title>
 	else
 		report=$($DPDK_CHECKPATCH_PATH $options - 2>/dev/null)
 	fi
-	[ $? -ne 0 ] || continue
+	[ $? -ne 0 ] || return 0
 	$verbose || printf '\n### %s\n\n' "$3"
 	printf '%s\n' "$report" | sed -n '1,/^total:.*lines checked$/p'
 	status=$(($status + 1))