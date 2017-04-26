70b85ee99bb0e1c5ef577dc746e4aaf86eccdfaf
Jan Blunck <jblunck@infradead.org>
2017-04-26 23:30:08 +0200

    bond: use rte_eth_dev_socket_id()
    The bonded device does not have to be a PCI device. Use the rte_ethdev
    functions instead.
    
    Signed-off-by: Wen Chiu <wchiu@brocade.com>
    Signed-off-by: Stephen Hemminger <stephen@networkplumber.org>
    Signed-off-by: Jan Blunck <jblunck@infradead.org>

--- drivers/net/bonding/rte_eth_bond_8023ad.c
+++ drivers/net/bonding/rte_eth_bond_8023ad.c
@@ -886,7 +886,7 @@ bond_mode_8023ad_activate_slave(struct rte_eth_dev *bond_dev, uint8_t slave_id)
 
 	RTE_ASSERT(port->rx_ring == NULL);
 	RTE_ASSERT(port->tx_ring == NULL);
-	socket_id = rte_eth_devices[slave_id].data->numa_node;
+	socket_id = rte_eth_dev_socket_id(slave_id);
 	if (socket_id == (uint8_t)LCORE_ID_ANY)
 		socket_id = 0;
 
