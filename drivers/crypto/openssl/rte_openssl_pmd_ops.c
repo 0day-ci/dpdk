0day-ci/dpdk

commit 8a9867a635c0572e926d87b7207dd064546388de

crypto/openssl: rename libcrypto to openssl
This patch replaces name "libcrypto" to "openssl" from file directories,
symbol prefixes and sub-names connected with old name.
Renamed poll mode driver files, test files, and documentations.
It is done to better name association with library because
the cryptography operations are using Openssl library crypto API.

Fixes: d61f70b4c918 ("crypto/libcrypto: add driver for OpenSSL library")

Signed-off-by: Slawomir Mrozowicz <slawomirx.mrozowicz@intel.com>
Acked-by: Deepak Kumar Jain <deepak.k.jain@intel.com>

drivers/crypto/openssl/rte_openssl_pmd_ops.c
@@ -36,10 +36,10 @@
 #include <rte_malloc.h>
 #include <rte_cryptodev_pmd.h>
 
-#include "rte_libcrypto_pmd_private.h"
+#include "rte_openssl_pmd_private.h"
 
 
-static const struct rte_cryptodev_capabilities libcrypto_pmd_capabilities[] = {
+static const struct rte_cryptodev_capabilities openssl_pmd_capabilities[] = {
 	{	/* MD5 HMAC */
 		.op = RTE_CRYPTO_OP_TYPE_SYMMETRIC,
 		{.sym = {
@@ -449,27 +449,27 @@ static const struct rte_cryptodev_capabilities libcrypto_pmd_capabilities[] = {
 
 /** Configure device */
 static int
-libcrypto_pmd_config(__rte_unused struct rte_cryptodev *dev)
+openssl_pmd_config(__rte_unused struct rte_cryptodev *dev)
 {
 	return 0;
 }
 
 /** Start device */
 static int
-libcrypto_pmd_start(__rte_unused struct rte_cryptodev *dev)
+openssl_pmd_start(__rte_unused struct rte_cryptodev *dev)
 {
 	return 0;
 }
 
 /** Stop device */
 static void
-libcrypto_pmd_stop(__rte_unused struct rte_cryptodev *dev)
+openssl_pmd_stop(__rte_unused struct rte_cryptodev *dev)
 {
 }
 
 /** Close device */
 static int
-libcrypto_pmd_close(__rte_unused struct rte_cryptodev *dev)
+openssl_pmd_close(__rte_unused struct rte_cryptodev *dev)
 {
 	return 0;
 }
@@ -477,13 +477,13 @@ libcrypto_pmd_close(__rte_unused struct rte_cryptodev *dev)
 
 /** Get device statistics */
 static void
-libcrypto_pmd_stats_get(struct rte_cryptodev *dev,
+openssl_pmd_stats_get(struct rte_cryptodev *dev,
 		struct rte_cryptodev_stats *stats)
 {
 	int qp_id;
 
 	for (qp_id = 0; qp_id < dev->data->nb_queue_pairs; qp_id++) {
-		struct libcrypto_qp *qp = dev->data->queue_pairs[qp_id];
+		struct openssl_qp *qp = dev->data->queue_pairs[qp_id];
 
 		stats->enqueued_count += qp->stats.enqueued_count;
 		stats->dequeued_count += qp->stats.dequeued_count;
@@ -495,12 +495,12 @@ libcrypto_pmd_stats_get(struct rte_cryptodev *dev,
 
 /** Reset device statistics */
 static void
-libcrypto_pmd_stats_reset(struct rte_cryptodev *dev)
+openssl_pmd_stats_reset(struct rte_cryptodev *dev)
 {
 	int qp_id;
 
 	for (qp_id = 0; qp_id < dev->data->nb_queue_pairs; qp_id++) {
-		struct libcrypto_qp *qp = dev->data->queue_pairs[qp_id];
+		struct openssl_qp *qp = dev->data->queue_pairs[qp_id];
 
 		memset(&qp->stats, 0, sizeof(qp->stats));
 	}
@@ -509,15 +509,15 @@ libcrypto_pmd_stats_reset(struct rte_cryptodev *dev)
 
 /** Get device info */
 static void
-libcrypto_pmd_info_get(struct rte_cryptodev *dev,
+openssl_pmd_info_get(struct rte_cryptodev *dev,
 		struct rte_cryptodev_info *dev_info)
 {
-	struct libcrypto_private *internals = dev->data->dev_private;
+	struct openssl_private *internals = dev->data->dev_private;
 
 	if (dev_info != NULL) {
 		dev_info->dev_type = dev->dev_type;
 		dev_info->feature_flags = dev->feature_flags;
-		dev_info->capabilities = libcrypto_pmd_capabilities;
+		dev_info->capabilities = openssl_pmd_capabilities;
 		dev_info->max_nb_queue_pairs = internals->max_nb_qpairs;
 		dev_info->sym.max_nb_sessions = internals->max_nb_sessions;
 	}
@@ -525,7 +525,7 @@ libcrypto_pmd_info_get(struct rte_cryptodev *dev,
 
 /** Release queue pair */
 static int
-libcrypto_pmd_qp_release(struct rte_cryptodev *dev, uint16_t qp_id)
+openssl_pmd_qp_release(struct rte_cryptodev *dev, uint16_t qp_id)
 {
 	if (dev->data->queue_pairs[qp_id] != NULL) {
 		rte_free(dev->data->queue_pairs[qp_id]);
@@ -536,11 +536,11 @@ libcrypto_pmd_qp_release(struct rte_cryptodev *dev, uint16_t qp_id)
 
 /** set a unique name for the queue pair based on it's name, dev_id and qp_id */
 static int
-libcrypto_pmd_qp_set_unique_name(struct rte_cryptodev *dev,
-		struct libcrypto_qp *qp)
+openssl_pmd_qp_set_unique_name(struct rte_cryptodev *dev,
+		struct openssl_qp *qp)
 {
 	unsigned int n = snprintf(qp->name, sizeof(qp->name),
-			"libcrypto_pmd_%u_qp_%u",
+			"openssl_pmd_%u_qp_%u",
 			dev->data->dev_id, qp->id);
 
 	if (n > sizeof(qp->name))
@@ -552,7 +552,7 @@ libcrypto_pmd_qp_set_unique_name(struct rte_cryptodev *dev,
 
 /** Create a ring to place processed operations on */
 static struct rte_ring *
-libcrypto_pmd_qp_create_processed_ops_ring(struct libcrypto_qp *qp,
+openssl_pmd_qp_create_processed_ops_ring(struct openssl_qp *qp,
 		unsigned int ring_size, int socket_id)
 {
 	struct rte_ring *r;
@@ -560,13 +560,13 @@ libcrypto_pmd_qp_create_processed_ops_ring(struct libcrypto_qp *qp,
 	r = rte_ring_lookup(qp->name);
 	if (r) {
 		if (r->prod.size >= ring_size) {
-			LIBCRYPTO_LOG_INFO(
+			OPENSSL_LOG_INFO(
 				"Reusing existing ring %s for processed ops",
 				 qp->name);
 			return r;
 		}
 
-		LIBCRYPTO_LOG_ERR(
+		OPENSSL_LOG_ERR(
 			"Unable to reuse existing ring %s for processed ops",
 			 qp->name);
 		return NULL;
@@ -579,18 +579,18 @@ libcrypto_pmd_qp_create_processed_ops_ring(struct libcrypto_qp *qp,
 
 /** Setup a queue pair */
 static int
-libcrypto_pmd_qp_setup(struct rte_cryptodev *dev, uint16_t qp_id,
+openssl_pmd_qp_setup(struct rte_cryptodev *dev, uint16_t qp_id,
 		const struct rte_cryptodev_qp_conf *qp_conf,
 		 int socket_id)
 {
-	struct libcrypto_qp *qp = NULL;
+	struct openssl_qp *qp = NULL;
 
 	/* Free memory prior to re-allocation if needed. */
 	if (dev->data->queue_pairs[qp_id] != NULL)
-		libcrypto_pmd_qp_release(dev, qp_id);
+		openssl_pmd_qp_release(dev, qp_id);
 
 	/* Allocate the queue pair data structure. */
-	qp = rte_zmalloc_socket("LIBCRYPTO PMD Queue Pair", sizeof(*qp),
+	qp = rte_zmalloc_socket("OPENSSL PMD Queue Pair", sizeof(*qp),
 					RTE_CACHE_LINE_SIZE, socket_id);
 	if (qp == NULL)
 		return -ENOMEM;
@@ -598,10 +598,10 @@ libcrypto_pmd_qp_setup(struct rte_cryptodev *dev, uint16_t qp_id,
 	qp->id = qp_id;
 	dev->data->queue_pairs[qp_id] = qp;
 
-	if (libcrypto_pmd_qp_set_unique_name(dev, qp))
+	if (openssl_pmd_qp_set_unique_name(dev, qp))
 		goto qp_setup_cleanup;
 
-	qp->processed_ops = libcrypto_pmd_qp_create_processed_ops_ring(qp,
+	qp->processed_ops = openssl_pmd_qp_create_processed_ops_ring(qp,
 			qp_conf->nb_descriptors, socket_id);
 	if (qp->processed_ops == NULL)
 		goto qp_setup_cleanup;
@@ -621,7 +621,7 @@ qp_setup_cleanup:
 
 /** Start queue pair */
 static int
-libcrypto_pmd_qp_start(__rte_unused struct rte_cryptodev *dev,
+openssl_pmd_qp_start(__rte_unused struct rte_cryptodev *dev,
 		__rte_unused uint16_t queue_pair_id)
 {
 	return -ENOTSUP;
@@ -629,7 +629,7 @@ libcrypto_pmd_qp_start(__rte_unused struct rte_cryptodev *dev,
 
 /** Stop queue pair */
 static int
-libcrypto_pmd_qp_stop(__rte_unused struct rte_cryptodev *dev,
+openssl_pmd_qp_stop(__rte_unused struct rte_cryptodev *dev,
 		__rte_unused uint16_t queue_pair_id)
 {
 	return -ENOTSUP;
@@ -637,31 +637,31 @@ libcrypto_pmd_qp_stop(__rte_unused struct rte_cryptodev *dev,
 
 /** Return the number of allocated queue pairs */
 static uint32_t
-libcrypto_pmd_qp_count(struct rte_cryptodev *dev)
+openssl_pmd_qp_count(struct rte_cryptodev *dev)
 {
 	return dev->data->nb_queue_pairs;
 }
 
 /** Returns the size of the session structure */
 static unsigned
-libcrypto_pmd_session_get_size(struct rte_cryptodev *dev __rte_unused)
+openssl_pmd_session_get_size(struct rte_cryptodev *dev __rte_unused)
 {
-	return sizeof(struct libcrypto_session);
+	return sizeof(struct openssl_session);
 }
 
 /** Configure the session from a crypto xform chain */
 static void *
-libcrypto_pmd_session_configure(struct rte_cryptodev *dev __rte_unused,
+openssl_pmd_session_configure(struct rte_cryptodev *dev __rte_unused,
 		struct rte_crypto_sym_xform *xform,	void *sess)
 {
 	if (unlikely(sess == NULL)) {
-		LIBCRYPTO_LOG_ERR("invalid session struct");
+		OPENSSL_LOG_ERR("invalid session struct");
 		return NULL;
 	}
 
-	if (libcrypto_set_session_parameters(
+	if (openssl_set_session_parameters(
 			sess, xform) != 0) {
-		LIBCRYPTO_LOG_ERR("failed configure session parameters");
+		OPENSSL_LOG_ERR("failed configure session parameters");
 		return NULL;
 	}
 
@@ -671,38 +671,38 @@ libcrypto_pmd_session_configure(struct rte_cryptodev *dev __rte_unused,
 
 /** Clear the memory of session so it doesn't leave key material behind */
 static void
-libcrypto_pmd_session_clear(struct rte_cryptodev *dev __rte_unused, void *sess)
+openssl_pmd_session_clear(struct rte_cryptodev *dev __rte_unused, void *sess)
 {
 	/*
 	 * Current just resetting the whole data structure, need to investigate
 	 * whether a more selective reset of key would be more performant
 	 */
 	if (sess) {
-		libcrypto_reset_session(sess);
-		memset(sess, 0, sizeof(struct libcrypto_session));
+		openssl_reset_session(sess);
+		memset(sess, 0, sizeof(struct openssl_session));
 	}
 }
 
-struct rte_cryptodev_ops libcrypto_pmd_ops = {
-		.dev_configure		= libcrypto_pmd_config,
-		.dev_start		= libcrypto_pmd_start,
-		.dev_stop		= libcrypto_pmd_stop,
-		.dev_close		= libcrypto_pmd_close,
+struct rte_cryptodev_ops openssl_pmd_ops = {
+		.dev_configure		= openssl_pmd_config,
+		.dev_start		= openssl_pmd_start,
+		.dev_stop		= openssl_pmd_stop,
+		.dev_close		= openssl_pmd_close,
 
-		.stats_get		= libcrypto_pmd_stats_get,
-		.stats_reset		= libcrypto_pmd_stats_reset,
+		.stats_get		= openssl_pmd_stats_get,
+		.stats_reset		= openssl_pmd_stats_reset,
 
-		.dev_infos_get		= libcrypto_pmd_info_get,
+		.dev_infos_get		= openssl_pmd_info_get,
 
-		.queue_pair_setup	= libcrypto_pmd_qp_setup,
-		.queue_pair_release	= libcrypto_pmd_qp_release,
-		.queue_pair_start	= libcrypto_pmd_qp_start,
-		.queue_pair_stop	= libcrypto_pmd_qp_stop,
-		.queue_pair_count	= libcrypto_pmd_qp_count,
+		.queue_pair_setup	= openssl_pmd_qp_setup,
+		.queue_pair_release	= openssl_pmd_qp_release,
+		.queue_pair_start	= openssl_pmd_qp_start,
+		.queue_pair_stop	= openssl_pmd_qp_stop,
+		.queue_pair_count	= openssl_pmd_qp_count,
 
-		.session_get_size	= libcrypto_pmd_session_get_size,
-		.session_configure	= libcrypto_pmd_session_configure,
-		.session_clear		= libcrypto_pmd_session_clear
+		.session_get_size	= openssl_pmd_session_get_size,
+		.session_configure	= openssl_pmd_session_configure,
+		.session_clear		= openssl_pmd_session_clear
 };
 
-struct rte_cryptodev_ops *rte_libcrypto_pmd_ops = &libcrypto_pmd_ops;
+struct rte_cryptodev_ops *rte_openssl_pmd_ops = &openssl_pmd_ops;

MAINTAINERS
@@ -437,10 +437,10 @@ M: Pablo de Lara <pablo.de.lara.guarch@intel.com>
 F: drivers/crypto/zuc/
 F: doc/guides/cryptodevs/zuc.rst
 
-LibCrypto PMD
+OpenSSL PMD
 M: Declan Doherty <declan.doherty@intel.com>
-F: drivers/crypto/libcrypto/
-F: doc/guides/cryptodevs/libcrypto.rst
+F: drivers/crypto/openssl/
+F: doc/guides/cryptodevs/openssl.rst
 
 Null Crypto PMD
 M: Declan Doherty <declan.doherty@intel.com>

app/test/test_cryptodev.c
@@ -326,24 +326,24 @@ testsuite_setup(void)
 		}
 	}
 
-	/* Create 2 LIBCRYPTO devices if required */
-	if (gbl_cryptodev_type == RTE_CRYPTODEV_LIBCRYPTO_PMD) {
-#ifndef RTE_LIBRTE_PMD_LIBCRYPTO
-		RTE_LOG(ERR, USER1, "CONFIG_RTE_LIBRTE_PMD_LIBCRYPTO must be"
+	/* Create 2 OPENSSL devices if required */
+	if (gbl_cryptodev_type == RTE_CRYPTODEV_OPENSSL_PMD) {
+#ifndef RTE_LIBRTE_PMD_OPENSSL
+		RTE_LOG(ERR, USER1, "CONFIG_RTE_LIBRTE_PMD_OPENSSL must be"
 			" enabled in config file to run this testsuite.\n");
 		return TEST_FAILED;
 #endif
 		nb_devs = rte_cryptodev_count_devtype(
-				RTE_CRYPTODEV_LIBCRYPTO_PMD);
+				RTE_CRYPTODEV_OPENSSL_PMD);
 		if (nb_devs < 2) {
 			for (i = nb_devs; i < 2; i++) {
 				ret = rte_eal_vdev_init(
-					RTE_STR(CRYPTODEV_NAME_LIBCRYPTO_PMD),
+					RTE_STR(CRYPTODEV_NAME_OPENSSL_PMD),
 					NULL);
 
 				TEST_ASSERT(ret == 0, "Failed to create "
 					"instance %u of pmd : %s", i,
-					RTE_STR(CRYPTODEV_NAME_LIBCRYPTO_PMD));
+					RTE_STR(CRYPTODEV_NAME_OPENSSL_PMD));
 			}
 		}
 	}
@@ -1482,14 +1482,14 @@ test_AES_chain_mb_all(void)
 }
 
 static int
-test_AES_chain_libcrypto_all(void)
+test_AES_chain_openssl_all(void)
 {
 	struct crypto_testsuite_params *ts_params = &testsuite_params;
 	int status;
 
 	status = test_blockcipher_all_tests(ts_params->mbuf_pool,
 		ts_params->op_mpool, ts_params->valid_devs[0],
-		RTE_CRYPTODEV_LIBCRYPTO_PMD,
+		RTE_CRYPTODEV_OPENSSL_PMD,
 		BLKCIPHER_AES_CHAIN_TYPE);
 
 	TEST_ASSERT_EQUAL(status, 0, "Test failed");
@@ -1498,14 +1498,14 @@ test_AES_chain_libcrypto_all(void)
 }
 
 static int
-test_AES_cipheronly_libcrypto_all(void)
+test_AES_cipheronly_openssl_all(void)
 {
 	struct crypto_testsuite_params *ts_params = &testsuite_params;
 	int status;
 
 	status = test_blockcipher_all_tests(ts_params->mbuf_pool,
 		ts_params->op_mpool, ts_params->valid_devs[0],
-		RTE_CRYPTODEV_LIBCRYPTO_PMD,
+		RTE_CRYPTODEV_OPENSSL_PMD,
 		BLKCIPHER_AES_CIPHERONLY_TYPE);
 
 	TEST_ASSERT_EQUAL(status, 0, "Test failed");
@@ -1530,14 +1530,14 @@ test_AES_chain_qat_all(void)
 }
 
 static int
-test_authonly_libcrypto_all(void)
+test_authonly_openssl_all(void)
 {
 	struct crypto_testsuite_params *ts_params = &testsuite_params;
 	int status;
 
 	status = test_blockcipher_all_tests(ts_params->mbuf_pool,
 		ts_params->op_mpool, ts_params->valid_devs[0],
-		RTE_CRYPTODEV_LIBCRYPTO_PMD,
+		RTE_CRYPTODEV_OPENSSL_PMD,
 		BLKCIPHER_AUTHONLY_TYPE);
 
 	TEST_ASSERT_EQUAL(status, 0, "Test failed");
@@ -3829,14 +3829,14 @@ test_3DES_cipheronly_qat_all(void)
 }
 
 static int
-test_3DES_chain_libcrypto_all(void)
+test_3DES_chain_openssl_all(void)
 {
 	struct crypto_testsuite_params *ts_params = &testsuite_params;
 	int status;
 
 	status = test_blockcipher_all_tests(ts_params->mbuf_pool,
 		ts_params->op_mpool, ts_params->valid_devs[0],
-		RTE_CRYPTODEV_LIBCRYPTO_PMD,
+		RTE_CRYPTODEV_OPENSSL_PMD,
 		BLKCIPHER_3DES_CHAIN_TYPE);
 
 	TEST_ASSERT_EQUAL(status, 0, "Test failed");
@@ -3845,14 +3845,14 @@ test_3DES_chain_libcrypto_all(void)
 }
 
 static int
-test_3DES_cipheronly_libcrypto_all(void)
+test_3DES_cipheronly_openssl_all(void)
 {
 	struct crypto_testsuite_params *ts_params = &testsuite_params;
 	int status;
 
 	status = test_blockcipher_all_tests(ts_params->mbuf_pool,
 		ts_params->op_mpool, ts_params->valid_devs[0],
-		RTE_CRYPTODEV_LIBCRYPTO_PMD,
+		RTE_CRYPTODEV_OPENSSL_PMD,
 		BLKCIPHER_3DES_CIPHERONLY_TYPE);
 
 	TEST_ASSERT_EQUAL(status, 0, "Test failed");
@@ -6190,8 +6190,8 @@ static struct unit_test_suite cryptodev_aesni_mb_testsuite  = {
 	}
 };
 
-static struct unit_test_suite cryptodev_libcrypto_testsuite  = {
-	.suite_name = "Crypto Device LIBCRYPTO Unit Test Suite",
+static struct unit_test_suite cryptodev_openssl_testsuite  = {
+	.suite_name = "Crypto Device OPENSSL Unit Test Suite",
 	.setup = testsuite_setup,
 	.teardown = testsuite_teardown,
 	.unit_test_cases = {
@@ -6199,15 +6199,15 @@ static struct unit_test_suite cryptodev_libcrypto_testsuite  = {
 		TEST_CASE_ST(ut_setup, ut_teardown,
 				test_multi_session_random_usage),
 		TEST_CASE_ST(ut_setup, ut_teardown,
-				test_AES_chain_libcrypto_all),
+				test_AES_chain_openssl_all),
 		TEST_CASE_ST(ut_setup, ut_teardown,
-				test_AES_cipheronly_libcrypto_all),
+				test_AES_cipheronly_openssl_all),
 		TEST_CASE_ST(ut_setup, ut_teardown,
-				test_3DES_chain_libcrypto_all),
+				test_3DES_chain_openssl_all),
 		TEST_CASE_ST(ut_setup, ut_teardown,
-				test_3DES_cipheronly_libcrypto_all),
+				test_3DES_cipheronly_openssl_all),
 		TEST_CASE_ST(ut_setup, ut_teardown,
-				test_authonly_libcrypto_all),
+				test_authonly_openssl_all),
 
 		/** AES GCM Authenticated Encryption */
 		TEST_CASE_ST(ut_setup, ut_teardown,
@@ -6520,11 +6520,11 @@ test_cryptodev_aesni_mb(void /*argv __rte_unused, int argc __rte_unused*/)
 }
 
 static int
-test_cryptodev_libcrypto(void)
+test_cryptodev_openssl(void)
 {
-	gbl_cryptodev_type = RTE_CRYPTODEV_LIBCRYPTO_PMD;
+	gbl_cryptodev_type = RTE_CRYPTODEV_OPENSSL_PMD;
 
-	return unit_test_suite_runner(&cryptodev_libcrypto_testsuite);
+	return unit_test_suite_runner(&cryptodev_openssl_testsuite);
 }
 
 static int
@@ -6569,7 +6569,7 @@ test_cryptodev_sw_zuc(void /*argv __rte_unused, int argc __rte_unused*/)
 
 REGISTER_TEST_COMMAND(cryptodev_qat_autotest, test_cryptodev_qat);
 REGISTER_TEST_COMMAND(cryptodev_aesni_mb_autotest, test_cryptodev_aesni_mb);
-REGISTER_TEST_COMMAND(cryptodev_libcrypto_autotest, test_cryptodev_libcrypto);
+REGISTER_TEST_COMMAND(cryptodev_openssl_autotest, test_cryptodev_openssl);
 REGISTER_TEST_COMMAND(cryptodev_aesni_gcm_autotest, test_cryptodev_aesni_gcm);
 REGISTER_TEST_COMMAND(cryptodev_null_autotest, test_cryptodev_null);
 REGISTER_TEST_COMMAND(cryptodev_sw_snow3g_autotest, test_cryptodev_sw_snow3g);

app/test/test_cryptodev_aes_test_vectors.h
@@ -831,7 +831,7 @@ static const struct blockcipher_test_case aes_chain_test_cases[] = {
 		.test_data = &aes_test_data_1,
 		.op_mask = BLOCKCIPHER_TEST_OP_ENC_AUTH_GEN,
 		.pmd_mask = BLOCKCIPHER_TEST_TARGET_PMD_MB |
-			BLOCKCIPHER_TEST_TARGET_PMD_LIBCRYPTO |
+			BLOCKCIPHER_TEST_TARGET_PMD_OPENSSL |
 			BLOCKCIPHER_TEST_TARGET_PMD_QAT
 	},
 	{
@@ -840,7 +840,7 @@ static const struct blockcipher_test_case aes_chain_test_cases[] = {
 		.test_data = &aes_test_data_1,
 		.op_mask = BLOCKCIPHER_TEST_OP_AUTH_VERIFY_DEC,
 		.pmd_mask = BLOCKCIPHER_TEST_TARGET_PMD_MB |
-			BLOCKCIPHER_TEST_TARGET_PMD_LIBCRYPTO |
+			BLOCKCIPHER_TEST_TARGET_PMD_OPENSSL |
 			BLOCKCIPHER_TEST_TARGET_PMD_QAT
 	},
 	{
@@ -862,7 +862,7 @@ static const struct blockcipher_test_case aes_chain_test_cases[] = {
 		.test_data = &aes_test_data_3,
 		.op_mask = BLOCKCIPHER_TEST_OP_ENC_AUTH_GEN,
 		.pmd_mask = BLOCKCIPHER_TEST_TARGET_PMD_MB |
-			BLOCKCIPHER_TEST_TARGET_PMD_LIBCRYPTO |
+			BLOCKCIPHER_TEST_TARGET_PMD_OPENSSL |
 			BLOCKCIPHER_TEST_TARGET_PMD_QAT
 	},
 	{
@@ -871,7 +871,7 @@ static const struct blockcipher_test_case aes_chain_test_cases[] = {
 		.test_data = &aes_test_data_3,
 		.op_mask = BLOCKCIPHER_TEST_OP_AUTH_VERIFY_DEC,
 		.pmd_mask = BLOCKCIPHER_TEST_TARGET_PMD_MB |
-			BLOCKCIPHER_TEST_TARGET_PMD_LIBCRYPTO |
+			BLOCKCIPHER_TEST_TARGET_PMD_OPENSSL |
 			BLOCKCIPHER_TEST_TARGET_PMD_QAT
 	},
 	{
@@ -879,7 +879,7 @@ static const struct blockcipher_test_case aes_chain_test_cases[] = {
 		.test_data = &aes_test_data_4,
 		.op_mask = BLOCKCIPHER_TEST_OP_ENC_AUTH_GEN,
 		.pmd_mask = BLOCKCIPHER_TEST_TARGET_PMD_MB |
-			BLOCKCIPHER_TEST_TARGET_PMD_LIBCRYPTO |
+			BLOCKCIPHER_TEST_TARGET_PMD_OPENSSL |
 			BLOCKCIPHER_TEST_TARGET_PMD_QAT
 	},
 	{
@@ -888,7 +888,7 @@ static const struct blockcipher_test_case aes_chain_test_cases[] = {
 		.test_data = &aes_test_data_4,
 		.op_mask = BLOCKCIPHER_TEST_OP_AUTH_VERIFY_DEC,
 		.pmd_mask = BLOCKCIPHER_TEST_TARGET_PMD_MB |
-			BLOCKCIPHER_TEST_TARGET_PMD_LIBCRYPTO |
+			BLOCKCIPHER_TEST_TARGET_PMD_OPENSSL |
 			BLOCKCIPHER_TEST_TARGET_PMD_QAT
 	},
 	{
@@ -896,7 +896,7 @@ static const struct blockcipher_test_case aes_chain_test_cases[] = {
 		.test_data = &aes_test_data_5,
 		.op_mask = BLOCKCIPHER_TEST_OP_ENC_AUTH_GEN,
 		.pmd_mask = BLOCKCIPHER_TEST_TARGET_PMD_MB |
-			BLOCKCIPHER_TEST_TARGET_PMD_LIBCRYPTO |
+			BLOCKCIPHER_TEST_TARGET_PMD_OPENSSL |
 			BLOCKCIPHER_TEST_TARGET_PMD_QAT
 	},
 	{
@@ -905,7 +905,7 @@ static const struct blockcipher_test_case aes_chain_test_cases[] = {
 		.test_data = &aes_test_data_5,
 		.op_mask = BLOCKCIPHER_TEST_OP_AUTH_VERIFY_DEC,
 		.pmd_mask = BLOCKCIPHER_TEST_TARGET_PMD_MB |
-			BLOCKCIPHER_TEST_TARGET_PMD_LIBCRYPTO |
+			BLOCKCIPHER_TEST_TARGET_PMD_OPENSSL |
 			BLOCKCIPHER_TEST_TARGET_PMD_QAT
 	},
 	{
@@ -913,7 +913,7 @@ static const struct blockcipher_test_case aes_chain_test_cases[] = {
 		.test_data = &aes_test_data_6,
 		.op_mask = BLOCKCIPHER_TEST_OP_ENC_AUTH_GEN,
 		.pmd_mask = BLOCKCIPHER_TEST_TARGET_PMD_MB |
-			BLOCKCIPHER_TEST_TARGET_PMD_LIBCRYPTO |
+			BLOCKCIPHER_TEST_TARGET_PMD_OPENSSL |
 			BLOCKCIPHER_TEST_TARGET_PMD_QAT
 	},
 	{
@@ -923,7 +923,7 @@ static const struct blockcipher_test_case aes_chain_test_cases[] = {
 		.op_mask = BLOCKCIPHER_TEST_OP_ENC_AUTH_GEN,
 		.feature_mask = BLOCKCIPHER_TEST_FEATURE_SESSIONLESS,
 		.pmd_mask = BLOCKCIPHER_TEST_TARGET_PMD_MB |
-			BLOCKCIPHER_TEST_TARGET_PMD_LIBCRYPTO
+			BLOCKCIPHER_TEST_TARGET_PMD_OPENSSL
 	},
 	{
 		.test_descr = "AES-128-CBC HMAC-SHA512 Decryption Digest "
@@ -931,7 +931,7 @@ static const struct blockcipher_test_case aes_chain_test_cases[] = {
 		.test_data = &aes_test_data_6,
 		.op_mask = BLOCKCIPHER_TEST_OP_AUTH_VERIFY_DEC,
 		.pmd_mask = BLOCKCIPHER_TEST_TARGET_PMD_MB |
-			BLOCKCIPHER_TEST_TARGET_PMD_LIBCRYPTO |
+			BLOCKCIPHER_TEST_TARGET_PMD_OPENSSL |
 			BLOCKCIPHER_TEST_TARGET_PMD_QAT
 	},
 	{
@@ -955,7 +955,7 @@ static const struct blockcipher_test_case aes_chain_test_cases[] = {
 		.op_mask = BLOCKCIPHER_TEST_OP_ENC_AUTH_GEN,
 		.feature_mask = BLOCKCIPHER_TEST_FEATURE_OOP,
 		.pmd_mask = BLOCKCIPHER_TEST_TARGET_PMD_QAT |
-			BLOCKCIPHER_TEST_TARGET_PMD_LIBCRYPTO
+			BLOCKCIPHER_TEST_TARGET_PMD_OPENSSL
 	},
 	{
 		.test_descr = "AES-128-CBC HMAC-SHA1 Decryption Digest "
@@ -964,14 +964,14 @@ static const struct blockcipher_test_case aes_chain_test_cases[] = {
 		.op_mask = BLOCKCIPHER_TEST_OP_AUTH_VERIFY_DEC,
 		.feature_mask = BLOCKCIPHER_TEST_FEATURE_OOP,
 		.pmd_mask = BLOCKCIPHER_TEST_TARGET_PMD_QAT |
-			BLOCKCIPHER_TEST_TARGET_PMD_LIBCRYPTO
+			BLOCKCIPHER_TEST_TARGET_PMD_OPENSSL
 	},
 	{
 		.test_descr = "AES-128-CBC HMAC-SHA224 Encryption Digest",
 		.test_data = &aes_test_data_8,
 		.op_mask = BLOCKCIPHER_TEST_OP_ENC_AUTH_GEN,
 		.pmd_mask = BLOCKCIPHER_TEST_TARGET_PMD_MB |
-			BLOCKCIPHER_TEST_TARGET_PMD_LIBCRYPTO |
+			BLOCKCIPHER_TEST_TARGET_PMD_OPENSSL |
 			BLOCKCIPHER_TEST_TARGET_PMD_QAT
 	},
 	{
@@ -980,7 +980,7 @@ static const struct blockcipher_test_case aes_chain_test_cases[] = {
 		.test_data = &aes_test_data_8,
 		.op_mask = BLOCKCIPHER_TEST_OP_AUTH_VERIFY_DEC,
 		.pmd_mask = BLOCKCIPHER_TEST_TARGET_PMD_MB |
-			BLOCKCIPHER_TEST_TARGET_PMD_LIBCRYPTO |
+			BLOCKCIPHER_TEST_TARGET_PMD_OPENSSL |
 			BLOCKCIPHER_TEST_TARGET_PMD_QAT
 	},
 	{
@@ -988,7 +988,7 @@ static const struct blockcipher_test_case aes_chain_test_cases[] = {
 		.test_data = &aes_test_data_9,
 		.op_mask = BLOCKCIPHER_TEST_OP_ENC_AUTH_GEN,
 		.pmd_mask = BLOCKCIPHER_TEST_TARGET_PMD_MB |
-			BLOCKCIPHER_TEST_TARGET_PMD_LIBCRYPTO |
+			BLOCKCIPHER_TEST_TARGET_PMD_OPENSSL |
 			BLOCKCIPHER_TEST_TARGET_PMD_QAT
 	},
 	{
@@ -997,7 +997,7 @@ static const struct blockcipher_test_case aes_chain_test_cases[] = {
 		.test_data = &aes_test_data_9,
 		.op_mask = BLOCKCIPHER_TEST_OP_AUTH_VERIFY_DEC,
 		.pmd_mask = BLOCKCIPHER_TEST_TARGET_PMD_MB |
-			BLOCKCIPHER_TEST_TARGET_PMD_LIBCRYPTO |
+			BLOCKCIPHER_TEST_TARGET_PMD_OPENSSL |
 			BLOCKCIPHER_TEST_TARGET_PMD_QAT
 	},
 	{
@@ -1006,7 +1006,7 @@ static const struct blockcipher_test_case aes_chain_test_cases[] = {
 		.test_data = &aes_test_data_4,
 		.op_mask = BLOCKCIPHER_TEST_OP_ENC_AUTH_GEN,
 		.feature_mask = BLOCKCIPHER_TEST_FEATURE_SESSIONLESS,
-		.pmd_mask = BLOCKCIPHER_TEST_TARGET_PMD_LIBCRYPTO
+		.pmd_mask = BLOCKCIPHER_TEST_TARGET_PMD_OPENSSL
 	},
 	{
 		.test_descr =
@@ -1015,7 +1015,7 @@ static const struct blockcipher_test_case aes_chain_test_cases[] = {
 		.test_data = &aes_test_data_4,
 		.op_mask = BLOCKCIPHER_TEST_OP_AUTH_VERIFY_DEC,
 		.feature_mask = BLOCKCIPHER_TEST_FEATURE_SESSIONLESS,
-		.pmd_mask = BLOCKCIPHER_TEST_TARGET_PMD_LIBCRYPTO
+		.pmd_mask = BLOCKCIPHER_TEST_TARGET_PMD_OPENSSL
 	},
 };
 
@@ -1024,73 +1024,73 @@ static const struct blockcipher_test_case aes_cipheronly_test_cases[] = {
 		.test_descr = "AES-128-CBC Encryption",
 		.test_data = &aes_test_data_4,
 		.op_mask = BLOCKCIPHER_TEST_OP_ENCRYPT,
-		.pmd_mask = BLOCKCIPHER_TEST_TARGET_PMD_LIBCRYPTO
+		.pmd_mask = BLOCKCIPHER_TEST_TARGET_PMD_OPENSSL
 	},
 	{
 		.test_descr = "AES-128-CBC Decryption",
 		.test_data = &aes_test_data_4,
 		.op_mask = BLOCKCIPHER_TEST_OP_DECRYPT,
-		.pmd_mask = BLOCKCIPHER_TEST_TARGET_PMD_LIBCRYPTO
+		.pmd_mask = BLOCKCIPHER_TEST_TARGET_PMD_OPENSSL
 	},
 	{
 		.test_descr = "AES-192-CBC Encryption",
 		.test_data = &aes_test_data_10,
 		.op_mask = BLOCKCIPHER_TEST_OP_ENCRYPT,
-		.pmd_mask = BLOCKCIPHER_TEST_TARGET_PMD_LIBCRYPTO
+		.pmd_mask = BLOCKCIPHER_TEST_TARGET_PMD_OPENSSL
 	},
 	{
 		.test_descr = "AES-192-CBC Decryption",
 		.test_data = &aes_test_data_10,
 		.op_mask = BLOCKCIPHER_TEST_OP_DECRYPT,
-		.pmd_mask = BLOCKCIPHER_TEST_TARGET_PMD_LIBCRYPTO
+		.pmd_mask = BLOCKCIPHER_TEST_TARGET_PMD_OPENSSL
 	},
 	{
 		.test_descr = "AES-256-CBC Encryption",
 		.test_data = &aes_test_data_11,
 		.op_mask = BLOCKCIPHER_TEST_OP_ENCRYPT,
-		.pmd_mask = BLOCKCIPHER_TEST_TARGET_PMD_LIBCRYPTO
+		.pmd_mask = BLOCKCIPHER_TEST_TARGET_PMD_OPENSSL
 	},
 	{
 		.test_descr = "AES-256-CBC Decryption",
 		.test_data = &aes_test_data_11,
 		.op_mask = BLOCKCIPHER_TEST_OP_DECRYPT,
-		.pmd_mask = BLOCKCIPHER_TEST_TARGET_PMD_LIBCRYPTO
+		.pmd_mask = BLOCKCIPHER_TEST_TARGET_PMD_OPENSSL
 	},
 	{
 		.test_descr = "AES-128-CTR Encryption",
 		.test_data = &aes_test_data_1,
 		.op_mask = BLOCKCIPHER_TEST_OP_ENCRYPT,
-		.pmd_mask = BLOCKCIPHER_TEST_TARGET_PMD_LIBCRYPTO
+		.pmd_mask = BLOCKCIPHER_TEST_TARGET_PMD_OPENSSL
 	},
 	{
 		.test_descr = "AES-128-CTR Decryption",
 		.test_data = &aes_test_data_1,
 		.op_mask = BLOCKCIPHER_TEST_OP_DECRYPT,
-		.pmd_mask = BLOCKCIPHER_TEST_TARGET_PMD_LIBCRYPTO
+		.pmd_mask = BLOCKCIPHER_TEST_TARGET_PMD_OPENSSL
 	},
 	{
 		.test_descr = "AES-192-CTR Encryption",
 		.test_data = &aes_test_data_2,
 		.op_mask = BLOCKCIPHER_TEST_OP_ENCRYPT,
-		.pmd_mask = BLOCKCIPHER_TEST_TARGET_PMD_LIBCRYPTO
+		.pmd_mask = BLOCKCIPHER_TEST_TARGET_PMD_OPENSSL
 	},
 	{
 		.test_descr = "AES-192-CTR Decryption",
 		.test_data = &aes_test_data_2,
 		.op_mask = BLOCKCIPHER_TEST_OP_DECRYPT,
-		.pmd_mask = BLOCKCIPHER_TEST_TARGET_PMD_LIBCRYPTO
+		.pmd_mask = BLOCKCIPHER_TEST_TARGET_PMD_OPENSSL
 	},
 	{
 		.test_descr = "AES-256-CTR Encryption",
 		.test_data = &aes_test_data_3,
 		.op_mask = BLOCKCIPHER_TEST_OP_ENCRYPT,
-		.pmd_mask = BLOCKCIPHER_TEST_TARGET_PMD_LIBCRYPTO
+		.pmd_mask = BLOCKCIPHER_TEST_TARGET_PMD_OPENSSL
 	},
 	{
 		.test_descr = "AES-256-CTR Decryption",
 		.test_data = &aes_test_data_3,
 		.op_mask = BLOCKCIPHER_TEST_OP_DECRYPT,
-		.pmd_mask = BLOCKCIPHER_TEST_TARGET_PMD_LIBCRYPTO
+		.pmd_mask = BLOCKCIPHER_TEST_TARGET_PMD_OPENSSL
 	},
 };
 

app/test/test_cryptodev_blockcipher.c
@@ -81,7 +81,7 @@ test_blockcipher_one_case(const struct blockcipher_test_case *t,
 
 	switch (cryptodev_type) {
 	case RTE_CRYPTODEV_QAT_SYM_PMD:
-	case RTE_CRYPTODEV_LIBCRYPTO_PMD:
+	case RTE_CRYPTODEV_OPENSSL_PMD:
 		digest_len = tdata->digest.len;
 		break;
 	case RTE_CRYPTODEV_AESNI_MB_PMD:
@@ -505,8 +505,8 @@ test_blockcipher_all_tests(struct rte_mempool *mbuf_pool,
 	case RTE_CRYPTODEV_QAT_SYM_PMD:
 		target_pmd_mask = BLOCKCIPHER_TEST_TARGET_PMD_QAT;
 		break;
-	case RTE_CRYPTODEV_LIBCRYPTO_PMD:
-		target_pmd_mask = BLOCKCIPHER_TEST_TARGET_PMD_LIBCRYPTO;
+	case RTE_CRYPTODEV_OPENSSL_PMD:
+		target_pmd_mask = BLOCKCIPHER_TEST_TARGET_PMD_OPENSSL;
 		break;
 	default:
 		TEST_ASSERT(0, "Unrecognized cryptodev type");

app/test/test_cryptodev_blockcipher.h
@@ -48,7 +48,7 @@
 
 #define BLOCKCIPHER_TEST_TARGET_PMD_MB		0x0001 /* Multi-buffer flag */
 #define BLOCKCIPHER_TEST_TARGET_PMD_QAT			0x0002 /* QAT flag */
-#define BLOCKCIPHER_TEST_TARGET_PMD_LIBCRYPTO	0x0004 /* SW LIBCRYPTO flag */
+#define BLOCKCIPHER_TEST_TARGET_PMD_OPENSSL	0x0004 /* SW OPENSSL flag */
 
 #define BLOCKCIPHER_TEST_OP_CIPHER	(BLOCKCIPHER_TEST_OP_ENCRYPT | \
 					BLOCKCIPHER_TEST_OP_DECRYPT)

commit f4ff2d60eea3a52ed624179d88b52aacae44d277

crypto/openssl: fix aad capabilities for AES-GCM
Fix aad capabilities for AES-GCM authtentication, aad is changed from
range 8-12 to 0-65535

Fixes: 8a9867a635c0 ("crypto/openssl: rename libcrypto to openssl")

Signed-off-by: Arek Kusztal <arkadiuszx.kusztal@intel.com>

drivers/crypto/openssl/rte_openssl_pmd_ops.c
@@ -350,9 +350,9 @@ static const struct rte_cryptodev_capabilities openssl_pmd_capabilities[] = {
 					.increment = 0
 				},
 				.aad_size = {
-					.min = 8,
-					.max = 12,
-					.increment = 4
+					.min = 0,
+					.max = 65535,
+					.increment = 1
 				}
 			}, }
 		}, }