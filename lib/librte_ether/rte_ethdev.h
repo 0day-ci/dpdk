04fc870e18fc733910ca6003de9dc5c2e3ab11f3
Boris Pismenny <borisp@mellanox.com>
2017-07-10 10:35:11 +0300

    ethdev: add device ipsec encrypt/decrypt capability flags
    A PMD that presents these flags supports IPsec crypto offload.
    Encryption and authentication on SA entries can be offloaded to
    the PMD. After adding a SA to hardware, crypto can be offloaded
    on well-formed IPsec packets.
    
    Signed-off-by: Boris Pismenny <borisp@mellanox.com>
    Signed-off-by: Aviad Yehezkel <aviadye@mellanox.com>

--- lib/librte_ether/rte_ethdev.h
+++ lib/librte_ether/rte_ethdev.h
@@ -883,6 +883,7 @@ struct rte_eth_conf {
 #define DEV_RX_OFFLOAD_QINQ_STRIP  0x00000020
 #define DEV_RX_OFFLOAD_OUTER_IPV4_CKSUM 0x00000040
 #define DEV_RX_OFFLOAD_MACSEC_STRIP     0x00000080
+#define DEV_RX_OFFLOAD_IPSEC_CRYPTO 0x00000100
 
 /**
  * TX offload capabilities of a device.
@@ -901,6 +902,9 @@ struct rte_eth_conf {
 #define DEV_TX_OFFLOAD_IPIP_TNL_TSO     0x00000800    /**< Used for tunneling packet. */
 #define DEV_TX_OFFLOAD_GENEVE_TNL_TSO   0x00001000    /**< Used for tunneling packet. */
 #define DEV_TX_OFFLOAD_MACSEC_INSERT    0x00002000
+#define DEV_TX_OFFLOAD_IPSEC_CRYPTO_HW_TRAILER 0x00004000
+#define DEV_TX_OFFLOAD_IPSEC_CRYPTO_TSO        0x00008000
+#define DEV_TX_OFFLOAD_IPSEC_CRYPTO_NEED_METADATA        0x00010000
 
 struct rte_pci_device;
 
